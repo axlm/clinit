c0f77d963aacce10d248c20ef9c2bb3edfc771dd
Axl Mattheus <axl.mattheus@gmail.com>
2017-02-10 13:43:20 +0200

    Added utility to get a contaner's ip.

--- fish/functions/docker-get-ip.fish
+++ fish/functions/docker-get-ip.fish
@@ -1,5 +1,4 @@
-function docker_get_ip_address
-    set container $argv[1]
-    sudo docker inspect --format='{{ .NetworkSettings.IPAddress }}' $container
+function docker-get-ip
+  set container $argv[1]
+  sudo docker inspect --format='{{ .NetworkSettings.IPAddress }}' $container
 end
-
