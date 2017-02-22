764d642e425328fe23602434a2b10af030da6efb
Axl Mattheus <axl.mattheus@gmail.com>
2017-02-22 18:34:38 +0200

    Added completion functionality. Removed redundant alias.

--- fish/completions/docker-attach-azk8s.fish
+++ fish/completions/docker-attach-azk8s.fish
@@ -1,1 +1,1 @@
-complete --command docker-attach-azk8s --exclusive --arguments "(ls ~/.az/acs/k8s | sed -e s'/\///')"
+complete --command docker-attach-azk8s --exclusive --arguments "(ls ~/.az/acs/k8s | sed -e s'/\///g')"
