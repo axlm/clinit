e214989328c10a1bc4b9b7ed6a150db3c6bd46d3
Axl Mattheus <axl.mattheus@gmail.com>
2017-02-22 16:52:44 +0200

    Moved one line functions to aliases.

--- fish/config.fish
+++ fish/config.fish
@@ -31,29 +31,63 @@ set --global --export PAGER   less
 set --global --export TERM    xterm-256color
 
 if status --is-interactive
-  # ALIASES
-  alias ..        cd\ ..
-  alias ...       cd\ ../..
-  alias ....      cd\ ../../..
-  alias .....     cd\ ../../../..
-  alias projects  cd\ $PROJECTS_HOME
-  alias annaxxko  cd\ $PROJECTS_HOME/annaxxKØ
-  alias axler8tor cd\ $PROJECTS_HOME/axler8tor
-  alias github    cd\ $PROJECTS_HOME/GitHub
-  alias downloads cd\ $HOME/Downloads
-  alias music     cd\ $HOME/Music
-  alias fishcfg   cd\ $HOME/.config/fish
-  alias bin       cd\ $HOME/.bin
-  alias lib       cd\ $HOME/.lib
-  # needed to let :q work in `insert mode`
-  alias :q        exit
-  alias clipboard xsel\ --input\ --clipboard
+    # ALIASES
+    alias ..        cd\ ..
+    alias ...       cd\ ../..
+    alias ....      cd\ ../../..
+    alias .....     cd\ ../../../..
+    alias projects  cd\ $PROJECTS_HOME
+    alias annaxxko  cd\ $PROJECTS_HOME/annaxxKØ
+    alias axler8tor cd\ $PROJECTS_HOME/axler8tor
+    alias github    cd\ $PROJECTS_HOME/GitHub
+    alias downloads cd\ $HOME/Downloads
+    alias music     cd\ $HOME/Music
+    alias fishcfg   cd\ $HOME/.config/fish
+    alias bin       cd\ $HOME/.bin
+    alias lib       cd\ $HOME/.lib
+    # needed to let :q work in `insert mode`
+    alias :q        exit
+    alias clipboard xsel\ --input\ --clipboard
+    # productivity
+    alias cp "command cp -uv"
+    alias df "command df -h"
+    alias dir "command dir --color=auto"
+    alias du "command du -h"
+    alias fgrep "command fgrep --color=auto"
+    alias grep "command grep --color=auto"
+    alias l "l1"
+    alias l1 "ls -1"
+    alias lS "ls -S"
+    alias la "ls -A"
+    alias ll "ls -l -k -h"
+    alias llS "ll -S"
+    alias lld "tree -ifd --noreport --dirsfirst"
+    alias llf "tree -if --noreport"
+    alias ls "command ls --color=auto -F --group-directories-first -X"
+    alias lsd "find (pwd) -mindepth 1 -maxdepth 1 -type d | sort"
+    alias lsf "find (pwd) -maxdepth 1 -type f | sort"
+    alias mv "command mv -uv"
+    alias ngrep "grep -v"
+    alias pgrep "command pgrep"
+    alias spacemacs "emacs -nw"
+    alias tma "tmux attach-session -t"
+    alias tmk "tmux kill-session -t"
+    alias tml "tmux list-sessions"
+    alias tms "tmux new-session -s"
+    alias tmtop "env TERM=screen-256color top"
+    alias tmvim "env TERM=screen-256color vim"
+    alias tmviml "env TERM=screen-256color SOLARIZED=LIGHT vim"
+    alias top "htop"
+    alias tree "command tree -F"
+    alias vim "vim -p"
 
-  # COLORS
-  dircolors --c-shell $HOME/.dircolors | source
+    # COLORS
+    dircolors --c-shell $HOME/.dircolors | source
 
-  # POWERLINE
-  set --global --export POWERLINE "ON"
-  set fish_function_path $fish_function_path "/home/axl/.local/lib/python3.5/site-packages/powerline/bindings/fish"
-  powerline-setup
+    # POWERLINE
+    set --global --export POWERLINE "ON"
+    set fish_function_path $fish_function_path "/home/axl/.local/lib/python3.5/site-packages/powerline/bindings/fish"
+    powerline-setup
 end
+
+
