ac960928046f81f1e4a82a71463c85954fec9a0b
Axl Mattheus <axl.mattheus@gmail.com>
2017-05-06 14:02:06 +0200

    Fixed clarg count.

--- fish/functions/docker-run.fish
+++ fish/functions/docker-run.fish
@@ -1,5 +1,5 @@
 function docker-run
-  if count $argv < 2
+  if test (count $argv) -lt 2
     docker run --interactive --tty --name "$argv[1]".(timestamp *) $argv[1]
   else
     docker run --interactive --tty --name "$argv[1]".(timestamp *) $argv[1] $argv[2]
