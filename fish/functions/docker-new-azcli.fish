bd12e86c023eb12f8c08fafd347ef299480ebfa4
Axl Mattheus <axl.mattheus@gmail.com>
2017-05-11 12:13:49 +0200

    Updated Azure CLI environment new function.

--- fish/functions/docker-new-azcli.fish
+++ fish/functions/docker-new-azcli.fish
@@ -4,49 +4,55 @@ function docker-new-azcli                          \
 
   # there must be a cluster name
   if test -z "$cli_"
-    printf "%s" "error: cli name not specified"
+    printf "%s\n" "error: cli name not specified"
     exit 127
   end
 
   set --local az_home_   "$HOME/.az"
-  set --local az_azqst_  "$az_home_/azqst"
+  set --local az_azqst_  "$az_home_/.azqst"
   set --local cli_home_  "$az_home_/$cli_"
 
+  # make sure the quickstart templates are up to date
   if not test -d "$az_azqst_"
+    printf "%s\n" "Pulling Azure Quick Start Templates."
+    printf "%s\n" "This may take a while..."
+
     mkdir -p "$az_azqst_"
     git clone https://github.com/Azure/azure-quickstart-templates.git "$az_azqst_"
   else
+    printf "%s\n" "Updating Azure Quick Start Templates."
+    printf "%s\n" "This may take a while..."
+
     pushd "$az_azqst_"
     git pull
     popd
   end
 
+  # create environement
   if not test -d "$cli_home_"
     mkdir -p "$cli_home_"
 
     pushd "$cli_home_"
-
     mkdir "azure"
-    mkdir "env"
+    mkdir "data"
     mkdir "kube"
-    mkdir "scripts"
     mkdir "ssh"
-
     ssh-keygen -t rsa -f "ssh/id_rsa" -q -N ""
-
     popd
   end
 
   # run the container
-  docker run                                               \
-    --interactive                                          \
-    --tty                                                  \
-    --volume "$az_azqst_:/root/azqst:ro"                   \
-    --volume "$cli_home_/azure:/root/.azure"               \
-    --volume "$cli_home_/env:/root/.env"                   \
-    --volume "$cli_home_/kube:/root/.kube"                 \
-    --volume "$cli_home_/script:/root/.scripts"            \
-    --volume "$cli_home_/ssh:/root/.ssh"                   \
-    --name "$cli_"                                         \
+  clear
+
+  docker run                                       \
+    --interactive                                  \
+    --tty                                          \
+    --rm                                           \
+    --volume "$az_azqst_:/root/azqst:ro"           \
+    --volume "$cli_home_/azure:/root/.azure"       \
+    --volume "$cli_home_/data:/root/data"          \
+    --volume "$cli_home_/kube:/root/.kube"         \
+    --volume "$cli_home_/ssh:/root/.ssh"           \
+    --name "$cli_"                                 \
     azuresdk/azure-cli-python:latest
 end
