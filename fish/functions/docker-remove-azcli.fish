6b51ca27da4b15459cb9d134d7c993dded125044
Axl Mattheus <axl.mattheus@gmail.com>
2017-05-11 12:13:29 +0200

    Updated Azure CLI environment remove function.

--- fish/functions/docker-remove-azcli.fish
+++ fish/functions/docker-remove-azcli.fish
@@ -1,9 +1,6 @@
 function docker-remove-azcli          \
   --argument-names cli_               \
   --description 'Remove an Azure CLI' \
-  
-  docker stop "$cli_"
-  docker rm "$cli_"
-  
+
   rm ~/.az/"$cli_"
 end
