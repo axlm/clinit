99cfdb011c5c4fa94c7da2a8cc1df08d230eee9b
Axl Mattheus <axl.mattheus@gmail.com>
2017-08-09 07:40:15 +0200

    Modified the image name generator.

--- fish/functions/docker-run-tmp.fish
+++ fish/functions/docker-run-tmp.fish
@@ -1,5 +1,5 @@
 function docker-run-tmp
-  set --local image (printf "%s" $argv[1] | sed -e 's/\//./g')
+  set --local image (printf "%s" $argv[1] | sed -e 's/\//./g' | sed -e 's/:/-t-/g')
   if test (count $argv) -lt 2
     docker run --interactive --tty --name "$image".(timestamp *) --rm $argv[1]
   else
