433ce109f8f61fe23f663cecbfebd4ee611d4a7b
Axl Mattheus <axl.mattheus@gmail.com>
2017-10-02 18:27:10 +0200

    Standardized indentation.

--- fish/functions/github-update.fish
+++ fish/functions/github-update.fish
@@ -1,9 +1,9 @@
 function github-update
-    for dir in (ls)
-        printf "$dir\n\n"
-        pushd $dir
-        git pull
-        printf "\n\n"
-        popd
-    end
+  for dir in (ls)
+    printf "$dir\n\n"
+    pushd $dir
+    git pull
+    printf "\n\n"
+    popd
+  end
 end
